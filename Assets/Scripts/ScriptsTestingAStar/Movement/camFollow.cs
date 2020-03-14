ec504aa7da3155adc62d0b5c107b947e31dfb76a
Unknown <gonalba@ucm.es>
2020-03-14 17:55:41 +0100

    player movement done

---
+++ Assets/Scripts/ScriptsTestingAStar/Movement/camFollow.cs
@@ -0,0 +1,24 @@
+﻿namespace UCM.IAV.Movimiento
+{
+
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class camFollow : MonoBehaviour
+    {
+        public GameObject personaje;
+        private Vector3 posicionRelativa;
+        // Start is called before the first frame update
+        public void Start()
+        {
+            posicionRelativa = transform.position - personaje.transform.position;
+        }
+
+        public void LateUpdate()
+        {
+            transform.position = personaje.transform.position + posicionRelativa;
+        }
+    }
+}
